7976f0b98cefe2af3f03055be8d7af883b656cd4
apple <apple@appledeMacBook-Air.local>
2019-02-05 13:18:20 +0800

    add

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './index.css';
+import { Icon } from 'antd-mobile';
 
 export default class Itmes extends React.PureComponent {
+    
+    constructor(props) {
+        super(props)
+
+    }
+
+    componentDidMount () {
+        
+    }
+
     render() {
         return (
-            <dic>
-                items
-            </dic>
+            <div className="item-page">
+                <div className='title'>
+                    <NavLink
+                        to='/'
+                    >title<Icon type='right' /></NavLink>
+                </div>
+                <div className='content'>
+                    <img src='' alt='' />
+                    <span className='desc'>描述信息</span>
+                </div>
+            </div>
         )
     }
 }
